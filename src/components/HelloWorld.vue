6d82b287f3e16727edfc3d24a45be97e6c826eec
袁秉瑞 <bingrui.yuan@roadshare.com>
2017-11-22 17:46:01 +0800

    repository init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ helloStr }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+  @Component
+  export default class HelloWorld extends Vue {
+    helloStr: string = "hello world!";
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
